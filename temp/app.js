ee3bea0f436fc58ad03d03065a060ff2b76284fa
Wenda <wendazhou3@gmail.com>
2019-02-21 14:47:18 +0000

    First commit

---
+++ temp/app.js
@@ -0,0 +1,91 @@
+var express=require('express');
+var mysql=require("./dbcon.js");
+
+
+var app =express();
+app.set("view engine", "ejs");
+app.set('mysql', mysql);
+
+var PORT=3306;
+
+app.get('/reset-table',function(req,res,next){
+  var context = {};
+  mysql.pool.query("DROP TABLE IF EXISTS todo", function(err){
+    var createString = "CREATE TABLE todo(" +
+    "id INT PRIMARY KEY AUTO_INCREMENT," +
+    "name VARCHAR(255) NOT NULL," +
+    "done BOOLEAN," +
+    "due DATE)";
+    mysql.pool.query(createString, function(err){
+      context.results = "Table reset";
+      res.render('home',context);
+    })
+  });
+});
+
+app.get('/insert',function(req,res,next){
+  var context = {};
+  mysql.pool.query("INSERT INTO todo (`name`) VALUES (?)", [req.query.c], function(err, result){
+    if(err){
+      next(err);
+      return;
+    }
+    context.results = "Inserted id " + result.insertId;
+    res.render('home',context);
+  });
+});
+
+app.get('/',function(req,res,next){
+  var context = {};
+  mysql.pool.query('SELECT * FROM todo', function(err, rows, fields){
+    if(err){
+      next(err);
+      return;
+    }
+    context.results = JSON.stringify(rows);
+    res.render('home', context);
+  });
+});
+
+
+app.get('/simple-update',function(req,res,next){
+  var context = {};
+  mysql.pool.query("UPDATE todo SET name=?, done=?, due=? WHERE id=? ",
+    [req.query.name, req.query.done, req.query.due, req.query.id],
+    function(err, result){
+    if(err){
+      next(err);
+      return;
+    }
+    context.results = "Updated " + result.changedRows + " rows.";
+    res.render('home',context);
+  });
+});
+
+
+app.get('/safe-update',function(req,res,next){
+  var context = {};
+  mysql.pool.query("SELECT * FROM todo WHERE id=?", [req.query.id], function(err, result){
+    if(err){
+      next(err);
+      return;
+    }
+    if(result.length == 1){
+      var curVals = result[0];
+      mysql.pool.query("UPDATE todo SET name=?, done=?, due=? WHERE id=? ",
+        [req.query.name || curVals.name, req.query.done || curVals.done, req.query.due || curVals.due, req.query.id],
+        function(err, result){
+        if(err){
+          next(err);
+          return;
+        }
+        context.results = "Updated " + result.changedRows + " rows.";
+        res.render('home',context);
+      });
+    }
+  });
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("The  Server Has Started!");
+});
