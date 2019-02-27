11bfac3cd8eb5d199cf691eed0ce9a89205d39a8
Wenda <wendazhou3@gmail.com>
2019-02-27 17:26:47 +0000

    ..

--- routes/user/awards.js
+++ routes/user/awards.js
@@ -4,7 +4,9 @@ var express    = require("express"),
     router     = express.Router();
     
     
-    
+router.get("/", function(req, res){
+    res.render("user/awards/show");
+});  
     
     
     
--- routes/user/profile.js
+++ routes/user/profile.js
@@ -4,7 +4,10 @@ var express    = require("express"),
     router     = express.Router();
     
     
-    
+router.get("/", function(req, res){
+    res.render("user/profile/show");
+});  
+        
     
     
     
