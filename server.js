4c666f313d509803b52ce861cb0dabade42169b3
Wenda <wendazhou3@gmail.com>
2019-02-28 03:25:25 +0000

    ..

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var express        = require("express"),
     mysql          = require('./dbcon.js'),
     app            = express();
 
-var PORT = 6599;
+var PORT = 6598;
 
 // requiring routes
 var indexRoutes     = require("./routes/index"),
