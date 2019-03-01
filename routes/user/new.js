77c988f2edfb2400d5ec7576993760cc28b6ef20
Wenda <wendazhou3@gmail.com>
2019-03-01 21:15:04 +0000

    ..

--- routes/user/new.js
+++ routes/user/new.js
@@ -8,8 +8,14 @@ router.get("/", middleware.isLoggedIn,  function(req, res){
     res.render("user/new/show");
 });  
         
-    
-    
+router.post("/", middleware.isLoggedIn, (req, res) => {
+    var redirect = "/user/new";
+    sql.createAward(req, res, sql.setNewAward, redirect);
+    // ERROR CHECK 1 - assure that the data sent in the requests body is not null
+    // ERROR CHECK 2 - assure that the user_id, award_id, and employee_id passed in corresponds to an entry in the database
+    // ERROR CHECK 3 - user must be logged in to get to this page
+	// ERROR CHECK 4 - user who is logged  in MUST NOT be an admin
+});
     
     
     
